6456e9e16ea3718c1f0db16a94702df403dac174
Truc Minh Phan <truc@trucphan.com>
2025-03-28 11:21:20 +0700

    chore: PR feedback

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,9 @@ import { builtinModules } from "node:module";
 import { resolve } from "node:path";
 import dts from "vite-plugin-dts";
 
-let NODE_BUILT_IN_MODULES = builtinModules.filter(
+const NODE_BUILT_IN_MODULES = builtinModules.filter(
   (m) => !m.startsWith("_") && ["fs", "fs/promises", "path", "url", "events", "stream", "string_decoder"].includes(m),
-);
-NODE_BUILT_IN_MODULES = [...NODE_BUILT_IN_MODULES.map((m) => `node:${m}`)];
+).map((m) => `node:${m}`);
 
 // # Build/Export: Vite Configuration
 export default defineConfig(({ command, mode }) => {
